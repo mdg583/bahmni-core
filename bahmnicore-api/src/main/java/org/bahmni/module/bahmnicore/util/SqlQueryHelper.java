f5608b1922d52b408d6931aa66a0a348ce722ef7
Matthew <mdg583@gmail.com>
2017-07-17 15:51:47 +0300

    Add support for repeated parameters for SQL queries. Note that behaviour is different, as previously all repeated parameters after the first would be ignored.

--- bahmnicore-api/src/main/java/org/bahmni/module/bahmnicore/util/SqlQueryHelper.java
+++ bahmnicore-api/src/main/java/org/bahmni/module/bahmnicore/util/SqlQueryHelper.java
@@ -1,10 +1,12 @@
 package org.bahmni.module.bahmnicore.util;
 
-import org.apache.log4j.Logger;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.bahmni.module.bahmnicore.model.searchParams.AdditionalSearchParam;
 import org.codehaus.jackson.map.ObjectMapper;
 
 import java.io.IOException;
+import java.lang.StringBuilder;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -17,7 +19,7 @@ import java.util.regex.Pattern;
 public class SqlQueryHelper {
     private final Pattern paramPlaceHolderPattern;
     private static final String PARAM_PLACE_HOLDER_REGEX = "\\$\\{[^{]*\\}";
-    private static final Logger log = Logger.getLogger(SqlQueryHelper.class);
+    private Log log = LogFactory.getLog(SqlQueryHelper.class);
 
     public SqlQueryHelper() {
         this.paramPlaceHolderPattern = Pattern.compile(PARAM_PLACE_HOLDER_REGEX);
@@ -40,6 +42,36 @@ public class SqlQueryHelper {
         return  queryString.replaceAll(PARAM_PLACE_HOLDER_REGEX,"?");
     }
 
+    private String getReplaceByParam(String[] param){
+        if(param.length == 1) return "?";
+        if(param.length > 1) {
+            StringBuilder ret = new StringBuilder(1+2*param.length);
+            ret.append('(');
+            for(int i = 0; i < param.length-1; i++){
+                ret.append("?,");
+            }
+            ret.append("?)");
+            return ret.toString();
+        }
+        return "";
+    }
+
+    private String expandPreparedStatementParameters(String statement, List<String> paramNamesFromPlaceHolders, Map<String, String[]> params) {
+        String ret = "";
+        int i = 0;
+        int n = 0;
+        int j = statement.indexOf('?', i);
+        while(j != -1){
+            String[] param = params.get(paramNamesFromPlaceHolders.get(n++));
+            String expanded = getReplaceByParam(param);
+            ret += statement.substring(i, j) + expanded;
+            i = j + 1;
+            j = statement.indexOf('?', i);
+        }
+        ret += statement.substring(i);
+        return ret;
+    }
+
     public PreparedStatement constructPreparedStatement(String queryString, Map<String, String[]> params, Connection conn) throws SQLException {
        String finalQueryString = queryString;
         if (params.get("additionalParams") != null && params.get("additionalParams") != null) {
@@ -48,14 +80,18 @@ public class SqlQueryHelper {
 
         List<String> paramNamesFromPlaceHolders = getParamNamesFromPlaceHolders(finalQueryString);
         String statement = transformIntoPreparedStatementFormat(finalQueryString);
+        statement = expandPreparedStatementParameters(statement, paramNamesFromPlaceHolders, params);
         PreparedStatement preparedStatement = conn.prepareStatement(statement);
-        if(params != null ){
-            int i=1;
-            for (String paramName : paramNamesFromPlaceHolders) {
-                String paramValue = params.get(paramName)[0];
-                preparedStatement.setObject(i++,paramValue);
+
+        int i = 1;
+        for (String paramName : paramNamesFromPlaceHolders) {
+            if(params.get(paramName) != null) {
+                for (String paramValue : params.get(paramName)) {
+                    preparedStatement.setObject(i++, paramValue);
+                }
             }
         }
+
         return preparedStatement;
     }
 
